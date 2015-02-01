ea6413887a7e79fd62549e1af226d2f60da2c1e2
Hugo Adolfo Perez <umwelt.hugo@gmail.com>
2015-02-01 21:56:17 +0100

    Ridership per station
    solution

--- project_5/riders_per_station/riders_per_station_reducer.py
+++ project_5/riders_per_station/riders_per_station_reducer.py
@@ -1,19 +1,43 @@
 import sys
+import logging
+
+from util import reducer_logfile
+logging.basicConfig(filename=reducer_logfile, format='%(message)s',
+                    level=logging.INFO, filemode='w')
 
 def reducer():
     '''
-    Given the output of the mapper for this exercise, the reducer should return one row
-    per unit, along with the total number of ENTRIESn_hourly over the course of may. 
-    
-    You can assume that the input to the reducer is sorted by UNIT, such that all rows 
-    corresponding to a particular UNIT are group together.
+    Given the output of the mapper for this exercise, the reducer should PRINT 
+    (not return) one line per UNIT along with the total number of ENTRIESn_hourly 
+    over the course of May (which is the duration of our data), separated by a tab.
+    An example output row from the reducer might look like this: 'R001\t500625.0'
 
+    You can assume that the input to the reducer is sorted such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    Since you are printing the output of your program, printing a debug 
+    statement will interfere with the operation of the grader. Instead, 
+    use the logging module, which we've configured to log to a file printed 
+    when you click "Test Run". For example:
+    logging.info("My debugging message")
     '''
-    
+
     entries = 0
     old_key = None
-
     for line in sys.stdin:
-        # your code here
+        data = line.strip().split("\t")
+        if len(data) !=2:
+            continue
 
+        key, count = data
+        if old_key and old_key != key:
+            print "{0}\t{1}".format(old_key,entries)
+            entries = 0
+        old_key = key
+        entries += float(count)
+
+    if old_key != None:
+        print "{0}\t{1}".format(old_key, entries)
+
+        
 reducer()
