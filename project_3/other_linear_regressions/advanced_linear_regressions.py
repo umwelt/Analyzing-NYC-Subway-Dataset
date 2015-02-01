b0eb8957a21a7661a35128914c9b9c3906acd9c9
Hugo Adolfo Perez <umwelt.hugo@gmail.com>
2015-02-01 21:36:16 +0100

    More Linear Regression (Optional)
    not enough

--- project_3/other_linear_regressions/advanced_linear_regressions.py
+++ project_3/other_linear_regressions/advanced_linear_regressions.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pandas as pd
+import pandas
 import scipy
-import statsmodels
+import statsmodels.api as sm
 
 """
 In this optional exercise, you should complete the function called 
@@ -9,32 +9,44 @@ predictions(turnstile_weather). This function takes in our pandas
 turnstile weather dataframe, and returns a set of predicted ridership values,
 based on the other information in the dataframe.  
 
-You should attempt to implement another type of linear regression, 
-that you may have read about, such as ordinary least squares regression: 
+In exercise 3.5 we used Gradient Descent in order to compute the coefficients
+theta used for the ridership prediction. Here you should attempt to implement 
+another way of computing the coeffcients theta. You may also try using a reference implementation such as: 
+http://statsmodels.sourceforge.net/devel/generated/statsmodels.regression.linear_model.OLS.html
+
+One of the advantages of the statsmodels implementation is that it gives you
+easy access to the values of the coefficients theta. This can help you infer relationships 
+between variables in the dataset.
+
+You may also experiment with polynomial terms as part of the input variables.  
+
+The following links might be useful: 
 http://en.wikipedia.org/wiki/Ordinary_least_squares
+http://en.wikipedia.org/w/index.php?title=Linear_least_squares_(mathematics)
+http://en.wikipedia.org/wiki/Polynomial_regression
 
 This is your playground. Go wild!
 
 How does your choice of linear regression compare to linear regression
-with gradient descent?
+with gradient descent computed in Exercise 3.5?
 
 You can look at the information contained in the turnstile_weather dataframe below:
 https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
 
 Note: due to the memory and CPU limitation of our amazon EC2 instance, we will
-give you a random subset (~15%) of the data contained in turnstile_data_master_with_weather.csv
+give you a random subset (~10%) of the data contained in turnstile_data_master_with_weather.csv
 
 If you receive a "server has encountered an error" message, that means you are hitting 
 the 30 second limit that's placed on running your program. See if you can optimize your code so it
 runs faster.
 """
-
+filename = "turnstile_data_master_with_weather.csv"
 def predictions(weather_turnstile):
-    #
     # Your implementation goes here. Feel free to write additional
-    # helper functions
-    # 
-    return prediction
+    y = weather_turnstile['ENTRIESn_hourly']
+    x = weather_turnstile[['EXITSn_hourly', 'Hour', 'maxpressurei','maxdewpti','mindewpti','minpressurei','meandewpti','meanpressurei','fog','rain','meanwindspdi','mintempi','meantempi', 'maxtempi', 'precipi', 'thunder']]
+    prediction = sm.OLS(y, x).fit()
+    return prediction.predict()
 
 def compute_r_squared(data, predictions):
     SST = ((data-np.mean(data))**2).sum()
