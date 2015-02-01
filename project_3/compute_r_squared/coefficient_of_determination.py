e53c11855cc0480abd9b6947d5edac2a8d45ecb5
Hugo Adolfo Perez <umwelt.hugo@gmail.com>
2015-02-01 21:30:10 +0100

    Rsquared
    solution

--- project_3/compute_r_squared/coefficient_of_determination.py
+++ project_3/compute_r_squared/coefficient_of_determination.py
@@ -1,19 +1,27 @@
-import pandas as pd
 import numpy as np
-
-from prediction import predictions
-
+import scipy
+import matplotlib.pyplot as plt
+import sys
 
 def compute_r_squared(data, predictions):
-    # Write a function that, given two input numpy arrays, 'data', and 'predictions,'
-    # returns the coefficient of determination, R^2, for the model that produced 
-    # predictions.
-    # 
-    # Numpy has a couple of functions -- np.mean() and np.sum() --
-    # that you might find useful, but you don't have to use them.
+    '''
+    In exercise 5, we calculated the R^2 value for you. But why don't you try and
+    and calculate the R^2 value yourself.
+    
+    Given a list of original data points, and also a list of predicted data points,
+    write a function that will compute and return the coefficient of determination (R^2)
+    for this data.  numpy.mean() and numpy.sum() might both be useful here, but
+    not necessary.
 
-    # YOUR CODE GOES HERE
-
+    Documentation about numpy.mean() and numpy.sum() below:
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.sum.html
+    '''
+    
+    # your code here
+    
+    r_squared = 1- (np.sum(np.square(data - predictions))) / np.sum(np.square(data-np.mean(data)))
+    
     return r_squared
 
 
